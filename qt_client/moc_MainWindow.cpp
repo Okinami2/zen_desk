cf01035d20bb33af0e288b5931e63c5f445d9e31
gemhack <cyb2890@163.com>
2026-05-30 23:02:04 +0800

    make qt_client for test on board on 2026.5.30

--- qt_client/moc_MainWindow.cpp
+++ qt_client/moc_MainWindow.cpp
@@ -201,8 +201,8 @@ void Overlay::clicked()
     QMetaObject::activate(this, &staticMetaObject, 0, nullptr);
 }
 struct qt_meta_stringdata_MainWindow_t {
-    QByteArrayData data[8];
-    char stringdata0[81];
+    QByteArrayData data[9];
+    char stringdata0[99];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -218,12 +218,13 @@ QT_MOC_LITERAL(3, 21, 9), // "showStats"
 QT_MOC_LITERAL(4, 31, 20), // "showStudySetupDialog"
 QT_MOC_LITERAL(5, 52, 10), // "startStudy"
 QT_MOC_LITERAL(6, 63, 7), // "minutes"
-QT_MOC_LITERAL(7, 71, 9) // "stopStudy"
+QT_MOC_LITERAL(7, 71, 9), // "stopStudy"
+QT_MOC_LITERAL(8, 81, 17) // "closeActiveDialog"
 
     },
     "MainWindow\0showHome\0\0showStats\0"
     "showStudySetupDialog\0startStudy\0minutes\0"
-    "stopStudy"
+    "stopStudy\0closeActiveDialog"
 };
 #undef QT_MOC_LITERAL
 
@@ -233,7 +234,7 @@ static const uint qt_meta_data_MainWindow[] = {
        8,       // revision
        0,       // classname
        0,    0, // classinfo
-       6,   14, // methods
+       7,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -241,18 +242,20 @@ static const uint qt_meta_data_MainWindow[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,   44,    2, 0x08 /* Private */,
-       3,    0,   45,    2, 0x08 /* Private */,
-       4,    0,   46,    2, 0x08 /* Private */,
-       5,    1,   47,    2, 0x08 /* Private */,
-       5,    0,   50,    2, 0x28 /* Private | MethodCloned */,
-       7,    0,   51,    2, 0x08 /* Private */,
+       1,    0,   49,    2, 0x08 /* Private */,
+       3,    0,   50,    2, 0x08 /* Private */,
+       4,    0,   51,    2, 0x08 /* Private */,
+       5,    1,   52,    2, 0x08 /* Private */,
+       5,    0,   55,    2, 0x28 /* Private | MethodCloned */,
+       7,    0,   56,    2, 0x08 /* Private */,
+       8,    0,   57,    2, 0x08 /* Private */,
 
  // slots: parameters
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void, QMetaType::Int,    6,
+    QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
 
@@ -271,6 +274,7 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         case 3: _t->startStudy((*reinterpret_cast< int(*)>(_a[1]))); break;
         case 4: _t->startStudy(); break;
         case 5: _t->stopStudy(); break;
+        case 6: _t->closeActiveDialog(); break;
         default: ;
         }
     }
@@ -305,13 +309,13 @@ int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 6)
+        if (_id < 7)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 6;
+        _id -= 7;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 6)
+        if (_id < 7)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 6;
+        _id -= 7;
     }
     return _id;
 }
